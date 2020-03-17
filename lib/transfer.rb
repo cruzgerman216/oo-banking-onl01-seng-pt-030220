30437de68aa44c0914a78c12d0b6bb5562c48981
German Cruz <cruzgerman216@yahoo.com>
2020-03-17 06:03:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,11 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :amount
   def initialize(name, bank_account, balance)
     @sender = name
     @receiver = bank_account
     @status = "Pending"
+    @amount = balance
   end
+
+  
 end
