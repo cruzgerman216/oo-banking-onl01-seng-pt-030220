3b75b36536b2fb280573f4f0b7540e558b471635
German Cruz <cruzgerman216@yahoo.com>
2020-03-17 08:04:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending" && @sender.balance > @amount && valid?
+    if @status == "pending" && @sender.balance > @amount && self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
